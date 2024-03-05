529c39e5229890932b4dd36d5cb066d9b4b71a9c
Luiz Augusto <96926902+luiz322@users.noreply.github.com>
2024-03-05 18:38:43 -0300

    Update script.js

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -1,26 +1,3 @@
-// Truthy and Falsy Values
-// 5 falsy values: 0, '', undefined, null, NaN
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Jonas'));
-console.log(Boolean({}));
-console.log(Boolean(''));
-
-const money = 100;
-if (money) {
-  console.log("Don't spend it all ;)");
-} else {
-  console.log('You should get a job!');
-}
-
-let height = 0;
-if (height) {
-  console.log('YAY! Height is defined');
-} else {
-  console.log('Height is UNDEFINED');
-}
-
-////////////////////////////////////
 // Equality Operators: == vs. ===
 const age = '18';
 if (age === 18) console.log('You just became an adult :D (strict)');
