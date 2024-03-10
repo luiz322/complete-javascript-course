8af0447cf5dd0dc20a988dd3dd90eac46d65448a
Luiz Augusto <96926902+luiz322@users.noreply.github.com>
2024-03-10 01:12:36 -0300

    Update script.js

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -1,21 +1,3 @@
-// Equality Operators: == vs. ===
-const favourite = Number(prompt("What's your favourite number?"));
-console.log(favourite);
-console.log(typeof favourite);
-
-if (favourite === 23) { // 22 === 23 -> FALSE
-  console.log('Cool! 23 is an amzaing number!')
-} else if (favourite === 7) {
-  console.log('7 is also a cool number')
-} else if (favourite === 9) {
-  console.log('9 is also a cool number')
-} else {
-  console.log('Number is not 23 or 7 or 9')
-}
-
-if (favourite !== 23) console.log('Why not 23?');
-
-////////////////////////////////////
 // Logical Operators
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
