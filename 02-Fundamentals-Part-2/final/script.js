e264f726ec9bb1bfd7cfb11aeef1201564244330
Luiz Augusto <96926902+luiz322@users.noreply.github.com>
2024-03-12 19:43:02 -0300

    Update script.js

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -1,19 +1,5 @@
 'use strict';
 
-/*
-///////////////////////////////////////
-// Activating Strict Mode
-let hasDriversLicense = false;
-const passTest = true;
-
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
-
-// const interface = 'Audio';
-// const private = 534;
-
-
-///////////////////////////////////////
 // Functions
 function logger() {
   console.log('My name is Jonas');
