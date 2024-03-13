bd729ea180730719261af993c0483289117947b5
Luiz Augusto <96926902+luiz322@users.noreply.github.com>
2024-03-13 20:39:10 -0300

    Update script.js

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -1,30 +1,3 @@
-'use strict';
-
-// Functions
-function logger() {
-  console.log('My name is Jonas');
-}
-
-// calling / running / invoking function
-logger();
-logger();
-logger();
-
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-const num = Number('23');
-
-
-///////////////////////////////////////
 // Function Declarations vs. Expressions
 
 // Function declaration
@@ -40,7 +13,6 @@ const calcAge2 = function (birthYeah) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
-
 
 ///////////////////////////////////////
 // Arrow functions
