02ab22801d6f4418075673ad5b6b54672c493898
Luiz Augusto <96926902+luiz322@users.noreply.github.com>
2024-03-16 10:52:30 -0300

    Update script.js

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -1,20 +1,4 @@
-// Arrow functions
 
-const calcAge3 = birthYeah => 2037 - birthYeah;
-const age3 = calcAge3(1991);
-console.log(age3);
-
-const yearsUntilRetirement = (birthYeah, firstName) => {
-  const age = 2037 - birthYeah;
-  const retirement = 65 - age;
-  // return retirement;
-  return `${firstName} retires in ${retirement} years`;
-}
-
-console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
-
-
-///////////////////////////////////////
 // Functions Calling Other Functions
 function cutFruitPieces(fruit) {
   return fruit * 4;
