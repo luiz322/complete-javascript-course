3132c1f06dfb8b7d55220c7b6c8fdaa53e1521ba
Luiz Augusto <96926902+luiz322@users.noreply.github.com>
2024-03-16 19:43:44 -0300

    Update script.js

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -1,20 +1,4 @@
 
-// Functions Calling Other Functions
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
-
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-
-  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
-console.log(fruitProcessor(2, 3));
-
-
-///////////////////////////////////////
 // Reviewing Functions
 const calcAge = function (birthYeah) {
   return 2037 - birthYeah;
