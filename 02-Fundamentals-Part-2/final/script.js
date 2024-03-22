36dff08a69f499ffccfe2e0043bf87ce813ae037
Luiz Augusto <96926902+luiz322@users.noreply.github.com>
2024-03-22 10:42:16 -0300

    Update script.js

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -1,21 +1,3 @@
-// Dot vs. Bracket Notation
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  age: 2037 - 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven']
-};
-console.log(jonas);
-
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
-
-const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
-
-// console.log(jonas.'last' + nameKey)
 
 const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
 
